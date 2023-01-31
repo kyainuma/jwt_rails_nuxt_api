b4c61e8440c2a8d73794aef146726e6a49c07725
kyainuma <yasu6491@icloud.com>
2023-01-31 21:37:33 +0900

    Add token generate module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Token生成モジュール
+  include TokenGenerateService
+
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 30, allow_blank: true }
