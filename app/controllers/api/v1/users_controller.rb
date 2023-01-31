ff1c76cdc2112f8c509d7913d1033b898c120b34
kyainuma <yasu6491@icloud.com>
2023-01-31 21:47:49 +0900

    Add UserAuthenticateService module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  # before_action :authenticate_user
+  before_action :authenticate_active_user
+
   def index
-    users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    # users = User.all
+    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  # Cookie
+  include ActionController::Cookies
+  # 認可
+  include UserAuthenticateService
 end
